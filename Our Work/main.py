5313c6855b1400adb73b30c5f8351e185e636288
Md shahadet Hossain <hossain.mdshahahdatca@gmail.com>
2023-03-25 14:16:28 -0600

    Python_9th

---
+++ Our Work/main.py
@@ -0,0 +1,79 @@
+#  Conditional Logic - Decision (Making)
+
+"""
+
+num
+
+> 0
+
+< 0
+
+==0
+
+"""
+
+"""
+1.Nums of Sc.
+2.Comparison Operators / Membership / Identity Operator
+"""
+
+
+# 01. If statement
+"""
+if condition :
+"""
+
+"""
+office_time = 9
+current_time = 8
+
+
+if office_time == current_time:
+    print("Go to Office")
+
+# 02. if else statement
+
+num = 10
+
+if num > 10:
+    print("Num is greater than 10")
+else:
+    print("Num is less than 10 or Equal")
+
+# 03. Nested if Statement
+
+num = 1000
+
+if num > 10:
+    print(">")
+if num < 10:
+    print("<")
+
+#  if num == 10:
+    print("==")
+#  else:
+#    print("=")
+"""
+#  04.  If..elif..else
+
+num = 5
+
+if num == 5 or num == 10:
+    print("==")
+    print("==")
+    print("==")
+    print("==")
+    print("==")
+elif num > 10:
+    print(">")
+elif num == 5:
+    print("5")
+else:
+    print("<")
+"""
+1. Short Answer 
+2. Multiple Choice 
+3. Multi-selection Choice  
+4. Output 
+"""
+
